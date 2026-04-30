df334fe46a5e1ba5d732353aa7d1f46af089e6c2
Rajat Gupta <rajatgupta3380@gmail.com>
2026-04-30 23:30:57 +0530

    A new print satement added

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 # This is my new file
 print('Hello')
-
+print('This is new print statement')
 
 # git add hello.py
 # git commit -m "added new python file"
-# git push -m origin main
+# git branch
+# git push -u origin main
+
+
+# make any other file
+# write some code into that
+# push that code on the github
+
+# git null
+# branching in git
